2336994864fa8d8d2d930ace49ee7662f4a76982
Brandon VanBuskirk <Brandonvb@hotmail.ca>
2021-11-08 12:44:44 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lists");
+            Console.WriteLine("(Inventory System)");
+            Console.WriteLine("");
+
+            string currentItem;
+            string[] inventory1 = new string[10];
+            // "[]" means an Array of strings. Will not be an Array without []
+            // init
+            //currentItem = "health potion";
+            currentItem = "smokes";
+            
+            inventory1[0] = "";
+            inventory1[1] = "";
+            inventory1[2] = "";
+            inventory1[3] = "";
+            for (int i = 0; i <= inventory1.Length - 1; i++) // 0-9 (for 10 items)
+            {
+                inventory1[i] = "";
+            }
+
+
+            // Simulated/Pretend gameplay
+            // player picks up "health potion
+            inventory1[0] = "health potion"; 
+
+            //player drops "health potion"
+
+
+
+
+
+
+            Console.WriteLine("Lists");
+            Console.WriteLine("(Inventory System)");
+            Console.WriteLine("");
+
+            List<string> inventory; // declaration
+            inventory = new List<string>(); // instantiation -- no size?
+
+            Console.WriteLine("Inventory size = " + inventory.Count);
+            // picks up "health potion"
+            inventory.Add("health potion"); // Could be a metheod
+            Console.WriteLine("Inventory size = " + inventory.Count);
+            inventory.Add("health potion");
+            Console.WriteLine("Inventory size = " + inventory.Count);
+
+
+
+
+
+            // show inventory?
+            //Console.WriteLine(inventory);
+            for (int i = 0; i <= inventory.Count - 1; i++) //0-?
+            {
+                //Console.WriteLine(inventory[i]);
+                // OR
+                Console.WriteLine(i + ": " + inventory[i]);
+
+
+
+                //try the difference
+            }
+            foreach (string item in inventory)
+            {
+                Console.WriteLine(item);
+            }
+
+            inventory.Remove("health potion");
+
+            Console.WriteLine("Inventory:");
+            foreach (string item in inventory)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+            
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
